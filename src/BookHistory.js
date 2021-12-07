87fb266e781a65b5546837155c51a9ae4bbe6bc4
keosha-w <keosha.watkins@asurion.com>
2021-12-07 10:45:41 -0600

    trying to get the content to show up again after adding auth elements

--- src/BookHistory.js
+++ src/BookHistory.js
@@ -1,13 +1,31 @@
+import { Redirect, Route } from "react-router"
 import { ApplicationViews } from "./ApplicationViews"
 import { NavBar } from "./components/nav/NavBar"
+import { Login } from "./components/auth/Login"
+import { Register } from "./components/auth/Register"
 
+export const BookHistory = () => (
+<>
+    <Route
+      render={() => {
+        if (localStorage.getItem("honey_customer")) {
+          return (
+            <>
+              <NavBar />
+              <ApplicationViews />
+            </>
+          );
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
 
-export const BookHistory = () => {
-    return (
-        <>
-            <NavBar />
-            <h1>bookHistory</h1>
-            <ApplicationViews />
-        </>
-    )
-}
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+  </>
+)
