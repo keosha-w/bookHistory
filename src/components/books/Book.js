230fb12f87c6dcafc949b1cb39d3532c6775adcf
keosha-w <keosha.watkins@asurion.com>
2021-12-10 22:43:01 -0600

    created DELETE fetch that works

--- src/components/books/Book.js
+++ src/components/books/Book.js
@@ -30,20 +30,25 @@ export default ({book}) => {
 
     
     const deleteBook = () => {
-        fetch(`http://localhost:8088/books${bookId}`, {
+        fetch(`http://localhost:8088/history/${bookId}`, {
             method: "DELETE"
         })
         .then(res => res.json())
         .then(() => {
-            history.pushState("/myBH")
+            history.push("/myBH")
         })
     }
 
     return(
         <>
+            <h3></h3>
             { details
 
-                ? <h4>{currentBook?.name} by {currentBook?.author}</h4>
+                ? <><h4>{currentBook?.name} by {currentBook?.author}</h4>
+                <p>{currentBook.description}</p>
+                <button>Edit</button>
+                <button onClick={deleteBook}>Delete</button>
+                </>
                 : <h4>{book?.name} by {book?.author}</h4>
             }
         </>
